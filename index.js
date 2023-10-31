81a7ef1286ed3c560f51c8d94d8a814044ad1b01
Connor Burns <cpburns17@gmail.com>
2023-10-31 15:59:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+// const returnFirstTwoDrivers = function(drivers) {
+//     return drivers.slice(0,2);
+// }
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+const returnLastTwoDrivers = function(drivers) {
+        return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer;
+    }
+}
+const fareDoubler = function(fare) {
+    return fare * 2;
+}
+const fareTripler = function(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, functions) {
+    return functions(arrayOfDrivers);
+
+}
